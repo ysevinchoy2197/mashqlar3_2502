638b976cfb5db568693527369bfd8c2cea1bf6c2
𝒮𝑒𝓋𝒾𝓃𝒸𝒽 <ysevinchoy2197@gmail.com>
2026-02-25 16:33:27 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,59 @@
+#1-misol
+def qoshish(a, b):
+    return a + b
+
+res = qoshish(6, 6)
+print(res)
+
+#2-misol
+def kvadrat(a, b):
+    return a * b
+print(kvadrat(2, 2))
+
+#3-misol
+def a(ism):
+    return len(ism)
+
+res = a("Sevinchoy")
+print(res)
+
+#4-misol
+def katta_kichik(a, b):
+    return max(a, b)
+print(katta_kichik(6, 5))
+
+#5-misol
+def matn(matn):
+    print(f"Sizning matningiz: {matn}")
+
+res = matn("HELLO")
+print(res)
+
+#6-misol
+def qaytarish(a, b):
+    return a - b
+
+res = qaytarish(8, 3)
+print(res)
+
+#7-misol
+def kop(a, b):
+    return a * b
+
+res = kop(4, 4)
+print(res)
+
+#8-misol
+def bolish(a, b):
+    return a // b
+
+res = bolish(8, 2)
+print(res)
+
+
+#9-misol
+def salom_ber(ism):
+    print(f"Salom, {ism}!")
+
+
+salom_ber("Sevinch")
